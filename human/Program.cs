bbca53520a2cb629c955f28c556a59878c4c45d6
David_Jr <35117601+davivaldi@users.noreply.github.com>
2019-09-04 11:18:20 -0600

    added default class values

--- human/Program.cs
+++ human/Program.cs
@@ -7,27 +7,31 @@ namespace human
         public int Strength;
         public int Intelligence;
         public int Dexterity;
-        private int health;
+        private int Health;
         public Human(string name){
             Name = name;
-            health=100;
+            Health = 100;
             Strength = 3;
-            Intelligence =3;
-            Dexterity =3;
+            Intelligence = 3;
+            Dexterity = 3;
         }
-        public string name
-    {
-        get { return Name; }
+        public Human(string name, int health, int strength, int intelligence, int dexterity)
+    {   
+        Name = name;
+        Health = health;
+        Strength = strength;
+        Intelligence = intelligence;
+        Dexterity = dexterity;
     }
 
         public int Attack(Human target){
-            target.health = target.health - Strength*5;
-            return target.health;
+            target.Health = target.Health - this.Strength*5;
+            return target.Health;
         }
 
 
 
-
+    
 
 
 
